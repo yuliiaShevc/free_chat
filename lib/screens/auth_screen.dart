576423122c43136c3b082c6f6a28f91639cb5668
Julia <ruzhytskya1997@gmail.com>
2021-10-15 00:21:24 +0300

    logic for phone validation + minor fix UI app bar

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:libphonenumber/libphonenumber.dart';
 import 'package:lottie/lottie.dart';
 
 import '../constants/strings.dart';
 
 class AuthScreen extends StatefulWidget {
   @override
-  _AuthScreenState createState() => _AuthScreenState();
+  State<AuthScreen> createState() => _AuthScreenState();
 }
 
 class _AuthScreenState extends State<AuthScreen> {
-  bool _phoneVerified = false;
+  final _form = new GlobalKey<FormState>();
+  String _verificationText;
+  TextEditingController _controller;
+
+  @override
+  void initState() {
+    _controller = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,36 +37,67 @@ class _AuthScreenState extends State<AuthScreen> {
           slivers: [
             SliverFillRemaining(
               hasScrollBody: false,
-              child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-                Lottie.asset("assets/lottie/enter_phone.json", height: 350),
-                Text(Strings.enterPhone, style: Theme.of(context).textTheme.headline2),
-                SizedBox(height: 40.0),
-                Container(
-                  height: Theme.of(context).buttonTheme.height,
-                  width: MediaQuery.of(context).size.width - 48.0,
-                  child: TextField(
-                      cursorHeight: 16.0,
-                      keyboardType: TextInputType.number,
-                      decoration: InputDecoration(
-                          floatingLabelBehavior: FloatingLabelBehavior.auto, hintText: Strings.hintTextPhoneInput),
-                      style: Theme.of(context).textTheme.caption),
-                ),
-                Expanded(child: Container()),
-                Padding(
-                  padding: const EdgeInsets.only(left: 24.0, right: 24.0, bottom: 54.0, top: 24.0),
-                  child: Container(
-                    height: Theme.of(context).buttonTheme.height,
-                    width: MediaQuery.of(context).size.width,
-                    child: TextButton(
-                        onPressed: () => setState(() => _phoneVerified = true),
-                        child: Text(Strings.continueBtn, style: Theme.of(context).textTheme.button)),
+              child: Container(
+                margin: const EdgeInsets.symmetric(horizontal: 24.0),
+                child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Lottie.asset("assets/lottie/enter_phone.json", height: 350),
+                  Text(Strings.enterPhone, style: Theme.of(context).textTheme.headline2),
+                  SizedBox(height: 16.0),
+                  Text(Strings.messageAuth,
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline4
+                          .copyWith(color: Theme.of(context).textTheme.headline4.color.withOpacity(0.8)),
+                      textAlign: TextAlign.center),
+                  SizedBox(height: 40.0),
+                  Form(
+                    key: _form,
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      child: TextFormField(
+                          controller: _controller,
+                          validator: (_) => _phoneVerificationNumber(),
+                          textInputAction: TextInputAction.done,
+                          onFieldSubmitted: (value) => _checkPhoneNumber(number: value),
+                          cursorHeight: 16.0,
+                          keyboardType: TextInputType.phone,
+                          decoration: InputDecoration(
+                              floatingLabelBehavior: FloatingLabelBehavior.auto, hintText: Strings.hintTextPhoneInput),
+                          style: Theme.of(context).textTheme.caption),
+                    ),
                   ),
-                ),
-              ]),
+                  Expanded(child: Container()),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 54.0, top: 24.0),
+                    child: Container(
+                      height: Theme.of(context).buttonTheme.height,
+                      width: MediaQuery.of(context).size.width,
+                      child: TextButton(
+                          onPressed: () => _checkPhoneNumber(),
+                          child: Text(Strings.continueBtn, style: Theme.of(context).textTheme.button)),
+                    ),
+                  ),
+                ]),
+              ),
             )
           ],
         ),
       ),
     );
+  }
+
+  Future<String> _phoneVerification(String number) async {
+    if (number.length == 0)
+      return Strings.errorEmptyMobileNumber;
+    else if (!(await PhoneNumberUtil.isValidPhoneNumber(phoneNumber: number, isoCode: 'US')))
+      return Strings.errorInvalidMobileNumber;
+    return null;
+  }
+
+  String _phoneVerificationNumber() => _verificationText;
+
+  void _checkPhoneNumber({String number}) async {
+    _verificationText = await _phoneVerification(number ?? _controller.text);
+    if (_form.currentState.validate()) return;
   }
 }
--- lib/theme.dart
+++ lib/theme.dart
@@ -4,6 +4,8 @@ import 'package:flutter/rendering.dart';
 import 'constants/colors.dart';
 
 ThemeData lightMyTheme() {
+  var errorInputBorder =
+      OutlineInputBorder(borderRadius: BorderRadius.circular(30.0), borderSide: BorderSide(width: 1.0, color: Colors.red));
   return ThemeData.light().copyWith(
       scaffoldBackgroundColor: Colors.white,
       primaryColor: Colors.black,
@@ -13,25 +15,26 @@ ThemeData lightMyTheme() {
       textSelectionTheme: TextSelectionThemeData(cursorColor: blue, selectionHandleColor: blue),
       buttonTheme: ButtonThemeData(height: 52.0),
       inputDecorationTheme: InputDecorationTheme(
+          focusedErrorBorder: errorInputBorder,
+          errorBorder: errorInputBorder,
           border: OutlineInputBorder(
               borderRadius: BorderRadius.circular(30.0), borderSide: BorderSide(width: 0, style: BorderStyle.none)),
           filled: true,
           fillColor: milk,
+          errorStyle: TextStyle(color: Colors.red, fontSize: 16.0, fontWeight: FontWeight.w400),
           contentPadding: EdgeInsets.only(left: 16.0, right: 16.0),
-          labelStyle: TextStyle(fontWeight: FontWeight.w400, color: Colors.black),
-          hintStyle: TextStyle(color: Color(0xffADB5BD), fontSize: 16.0, fontWeight: FontWeight.w600)),
+          hintStyle: TextStyle(color: hintTextColorLight, fontSize: 16.0, fontWeight: FontWeight.w600)),
       textButtonTheme: TextButtonThemeData(
           style: TextButton.styleFrom(
               backgroundColor: blue,
               shape: (RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(30.0)))),
               primary: milk,
-              textStyle: TextStyle(color: milk, fontSize: 16, fontWeight: FontWeight.w400))),
+              textStyle: TextStyle(color: milk, fontSize: 16.0, fontWeight: FontWeight.w400))),
       textTheme: TextTheme(
-              headline2: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
-              bodyText1: TextStyle(fontSize: 24, fontWeight: FontWeight.w500, color: Colors.black),
-              bodyText2: TextStyle(fontSize: 16, fontWeight: FontWeight.w400, color: Colors.black54),
-              caption: TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
-              button: TextStyle(fontSize: 16, fontWeight: FontWeight.w600, color: milk))
+              headline2: TextStyle(fontSize: 24.0, fontWeight: FontWeight.w700),
+              headline4: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
+              caption: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w600),
+              button: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w600, color: milk))
           .apply(fontFamily: "Mulish", bodyColor: milk, displayColor: scaffoldBackgroundColor));
 }
 
@@ -40,27 +43,29 @@ ThemeData darkMyTheme() {
       scaffoldBackgroundColor: scaffoldBackgroundColor,
       primaryColor: Colors.white,
       iconTheme: IconThemeData(color: milk),
-      appBarTheme: AppBarTheme(iconTheme: IconThemeData(color: milk), elevation: 0, backgroundColor: Colors.white),
+      appBarTheme: AppBarTheme(iconTheme: IconThemeData(color: milk), elevation: 0, backgroundColor: scaffoldBackgroundColor),
       textSelectionTheme: TextSelectionThemeData(cursorColor: milk, selectionHandleColor: milk),
       inputDecorationTheme: InputDecorationTheme(
+          focusedErrorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30.0), borderSide: BorderSide(width: 1.0, color: Colors.red)),
+          errorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30.0), borderSide: BorderSide(width: 1.0, color: Colors.red)),
           border: OutlineInputBorder(
               borderRadius: BorderRadius.circular(30.0), borderSide: BorderSide(width: 0, style: BorderStyle.none)),
           filled: true,
           fillColor: textInputBgColorDark,
           contentPadding: EdgeInsets.only(left: 16.0, right: 16.0),
-          labelStyle: TextStyle(fontWeight: FontWeight.w400, color: milk),
-          hintStyle: TextStyle(color: milk, fontSize: 16.0, fontWeight: FontWeight.w600)),
+          hintStyle: TextStyle(color: milk.withOpacity(0.6), fontSize: 16.0, fontWeight: FontWeight.w600)),
       textButtonTheme: TextButtonThemeData(
           style: TextButton.styleFrom(
               backgroundColor: blueDarkMode,
               shape: (RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(30.0)))),
-              textStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.w400, letterSpacing: 0.8))),
+              textStyle: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w400, letterSpacing: 0.8))),
       buttonTheme: ButtonThemeData(height: 48.0),
       textTheme: TextTheme(
-              headline2: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
-              bodyText1: TextStyle(fontSize: 24, fontWeight: FontWeight.w500, color: Colors.white),
-              bodyText2: TextStyle(fontSize: 16, fontWeight: FontWeight.w400, color: Colors.white54),
-              caption: TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
-              button: TextStyle(fontSize: 16, fontWeight: FontWeight.w600, color: milk))
+              headline2: TextStyle(fontSize: 24.0, fontWeight: FontWeight.w700),
+              headline4: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
+              caption: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w600),
+              button: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w600, color: milk))
           .apply(fontFamily: "Mulish", bodyColor: milk, displayColor: milk));
 }
